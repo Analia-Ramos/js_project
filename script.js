6254cf6f251b449ff61e45e6d4bd53044c66b35e
analia <analia.ramos13@hotmail.com>
2022-11-08 21:15:58 -0300

    css mejorado, js array, funciones y objetos

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ while (password != input){
 }
 
 
-//precios de habitaciones
+// //precios de habitaciones
 
 let sharedRoom = 20;
 let girlsRoom = 30;
@@ -36,13 +36,13 @@ let chooseRoom = prompt("Choose your room");
 let rate = chooseRoom;
 
 function roomRate (rate){
-    if ((rate == "Shared Room") || (rate == "shared room") || (rate == "shared")){
+    if ((rate === "Shared Room") || (rate === "shared room") || (rate === "shared")){
         alert("Shared room rate is $" + sharedRoom + " per night");
-            
-    }else if ((rate == "Girls Room") || (rate == "girls room") || (rate == "girls")){
+
+    }else if ((rate === "Girls Room") || (rate === "girls room") || (rate === "girls")){
         alert("Girls room rate is $" + girlsRoom + " per night");
 
-    }else if ((rate == "Private Room") || (rate == "private room") || (rate == "private")){
+    }else if ((rate === "Private Room") || (rate == "private room") || (rate === "private")){
         alert("Private room rate is $" + privateRoom + " per night");
 
     }else{
@@ -65,3 +65,55 @@ function roomRate (rate){
 }
 roomRate(rate);
 
+//Class constructor
+class Rooms {
+    constructor(id,name,price, availability){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.availability = availability;
+    }
+}
+
+const roomS = new Rooms (001, "Shared room",20,true);
+const roomG = new Rooms (002,"Girls Room",30,true);
+const roomP = new Rooms (003, "Private Room",40,false);
+
+//ddbb de habitaciones
+const BBDD = [roomS,roomG,roomP];
+
+//id - name - price - availability 
+BBDD.forEach((el)=>{
+    console.log(el)
+})
+
+//confirmando reserva de habitación
+let bookRoom = [];
+
+let room = Number(prompt("Would you like to make a reservation? Choose 1 for Shared room - 2 for Girls room - 3 for Private room - 4 Not at the moment"));
+
+let reserveRoom = room;
+
+
+function booking(reserveRoom){
+    if(reserveRoom === 1) {
+        alert("There are 2 Shared rooms available");
+
+    } else if (reserveRoom === 2) {
+        alert("There are 2 girls room available");
+
+    } else if (reserveRoom === 3) {
+        alert("Sorry, Private rooms are all booked out")
+    
+    } else if (reserveRoom === 4){
+        alert("We hope to see you soon!")
+
+    } else {
+        Number(prompt("Please, choose the options 1, 2, 3 or 4"));
+    }
+
+    bookRoom.push(BBDD[reserveRoom]);
+}
+booking(reserveRoom);
+
+console.log(reserveRoom);
